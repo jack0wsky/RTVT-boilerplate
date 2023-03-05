3243c2f4127730eaeb1260fa4c1b3174fcf30a3d
Jacek Łudzik <jacek.ludzik@elpassion.pl>
2023-03-05 18:04:22 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./app.css";
+
+function App() {
+  return (
+    <div className="flex justify-center items-center w-screen h-screen">
+      <h1 className="text-2xl">React + Vite + Tailwind boilerplate</h1>
+    </div>
+  );
+}
+
+export default App;
